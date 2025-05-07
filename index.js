f8ddc5875f4a253339131d682f26a8ea1d8351d4
sreeraj <117061390+sreeraj2004@users.noreply.github.com>
2025-05-07 21:13:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 5000
 app.get('/',(req,res)=>{
     res.send(`<h1>Hello from jenkins</h1>`)
 })
 
 
 app.listen(5000 , ()=>{
-    console.log('Server is listening at port 5000');
+    console.log(`Server is listening at port ${port}`);
 })
