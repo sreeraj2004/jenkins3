26f4c4f93fff390aa236ff09a2248264a6064f49
sreeraj <117061390+sreeraj2004@users.noreply.github.com>
2025-05-07 21:34:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000
 app.get('/',(req,res)=>{
-    res.send(`<h1>Hello from jenkins</h1> `)
+    res.send(`<h1>Hello from jenkins</h1>`)
 })
 
 
